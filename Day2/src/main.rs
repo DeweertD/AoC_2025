2fb54c4bccb35fc338076c1e6e8245b0a3d62a5e
Devon Deweert <25532977+DeweertD@users.noreply.github.com>
2025-12-18 16:07:49 -0700

    D2P2

--- Day2/src/main.rs
+++ Day2/src/main.rs
@@ -36,6 +36,31 @@ Your job is to find all of the invalid IDs that appear in the given ranges. In t
 Adding up all the invalid IDs in this example produces 1227775554.
 
 What do you get if you add up all of the invalid IDs?
+
+--- Part Two ---
+
+The clerk quickly discovers that there are still invalid IDs in the ranges in your list. Maybe the young Elf was doing other silly patterns as well?
+
+Now, an ID is invalid if it is made only of some sequence of digits repeated at least twice. So, 12341234 (1234 two times), 123123123 (123 three times), 1212121212 (12 five times), and 1111111 (1 seven times) are all invalid IDs.
+
+From the same example as before:
+
+    11-22 still has two invalid IDs, 11 and 22.
+    95-115 now has two invalid IDs, 99 and 111.
+    998-1012 now has two invalid IDs, 999 and 1010.
+    1188511880-1188511890 still has one invalid ID, 1188511885.
+    222220-222224 still has one invalid ID, 222222.
+    1698522-1698528 still contains no invalid IDs.
+    446443-446449 still has one invalid ID, 446446.
+    38593856-38593862 still has one invalid ID, 38593859.
+    565653-565659 now has one invalid ID, 565656.
+    824824821-824824827 now has one invalid ID, 824824824.
+    2121212118-2121212124 now has one invalid ID, 2121212121.
+
+Adding up all the invalid IDs in this example produces 4174379265.
+
+What do you get if you add up all of the invalid IDs using these new rules?
+
 */
 use std::iter::Map;
 use std::str;
@@ -43,7 +68,7 @@ use std::str;
 fn main() {
     let file = std::fs::read_to_string("Day2/input.txt").expect("file not found or read");
     println!("Day 2 Part 1: {}", part1(read_values(&file)));
-    // println!("Day 2 Part 2: {}", part2(read_values(&file)));
+    println!("Day 2 Part 2: {}", part2(read_values(&file)));
 }
 
 fn part1(values: Map<str::Split<'_, char>, fn(&str) -> (u64, u64)>) -> u64 {
@@ -85,8 +110,43 @@ fn part1(values: Map<str::Split<'_, char>, fn(&str) -> (u64, u64)>) -> u64 {
     invalid_id_accumulator
 }
 
-fn _part2(_values: Map<str::Split<'_, char>, fn(&str) -> (u64, u64)>) -> i32 {
-    todo!()
+fn part2(values: Map<str::Split<'_, char>, fn(&str) -> (u64, u64)>) -> u64 {
+    let mut invalid_id_accumulator = 0;
+    values.for_each(|(lower, upper)| {
+        let mut lower_half = 0;
+        let mut upper_half = 0;
+
+        for i in 0..=1 {
+            let lower_zeros = (count_digits(lower) / 2) + i;
+            let lower_divisor = 10u64.pow(lower_zeros) + 1;
+            lower_half = (lower as f64 / lower_divisor as f64).ceil() as u64;
+            if lower_half <= lower_divisor {
+                break;
+            };
+        }
+
+        for i in 0..1 {
+            let upper_zeros = (count_digits(upper) / 2) + i;
+            let upper_divisor = 10u64.pow(upper_zeros) + 1;
+            upper_half = (upper as f64 / upper_divisor as f64).floor() as u64;
+            if upper_half <= upper_divisor {
+                break;
+            };
+        }
+
+        for i in lower_half..=upper_half {
+            let doubling_factor = 10u64.pow(count_digits(i)) + 1;
+            let invalid_id = i * doubling_factor;
+            if invalid_id < lower {
+                continue;
+            }
+            if invalid_id > upper {
+                break;
+            }
+            invalid_id_accumulator += invalid_id;
+        }
+    });
+    invalid_id_accumulator
 }
 
 fn read_values(file: &String) -> Map<str::Split<'_, char>, fn(&str) -> (u64, u64)> {
